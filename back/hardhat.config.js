f59a8c6253520bd1af85af1b0184f30051a85719
Wim Stalmans <wim.stalmans@gmail.com>
2024-04-19 08:11:12 +0200

    initialisation process description

--- back/hardhat.config.js
+++ back/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // As the first line of the file
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ETHSCAN_API,
+      accounts: [`0x${process.env.PRIVATE_KEY}`] // Use environment variable to keep your private key secure
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHSCAN_API
+  },
+  sourcify: {
+    enabled: true
+  }
 };
